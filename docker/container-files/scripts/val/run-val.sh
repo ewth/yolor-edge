98f48dd2d1ee5214003c7aecf624f8e956ca21b0
Ewan Thompson <ewanpt@gmail.com>
2021-11-25 20:03:44 +0800

    Take wandb stuff out of container script; cleanup

--- docker/container-files/scripts/val/run-val.sh
+++ docker/container-files/scripts/val/run-val.sh
@@ -7,17 +7,12 @@
 
 # Thanks https://stackoverflow.com/questions/59895/
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source ${SCRIPT_DIR}/../common/checks.sh
-if [[ -z "${IN_DOCKER}" || !${IN_DOCKER} ]]; then
-    echo "Please run from inside the docker container only."
-    exit
-fi
 
 if [[ -z "${BATCH_SIZE}" ]]; then
-    BATCH_SIZE=4
+    BATCH_SIZE=20
 fi
 if [[ -z "${IMAGE_SIZE}" ]]; then
-    IMAGE_SIZE=640
+    IMAGE_SIZE=256
 fi
 
 if [[ -z "${YOLOR_VERSION}" ]]; then
@@ -27,14 +22,16 @@ fi
 echo "Starting validation of ${YOLOR_VERSION} with batch size ${BATCH_SIZE}, image size ${IMAGE_SIZE}"
 
 # This is just read to pass to wandb
+EXTRA_CMD=""
 READ_SHM=$(cat /proc/mounts | grep "/dev/shm")
 READ_SHM=$(echo "$READ_SHM" | grep -P 'size=([0-9]+[a-zA-Z])' -o)
 SHM_SIZE=$(echo "$READ_SHM" | grep -P '([0-9]+[a-zA-Z])' -o)
-if [[ ! $SHM_SIZE =~ [0-9]+[a-zA-Z] ]]; then
+if [[ !$SHM_SIZE =~ [0-9]+[a-zA-Z] ]]; then
     SHM_SIZE=""
 fi
 
-WANDB_PROJECT="jetson-yolor" WANDB_ENTITY="ewth" SHM_SIZE=${SHM_SIZE} python3 /yolor/test.py \
+
+SHM_SIZE=${SHM_SIZE} python3 /yolor/test.py \
     --data /jetson-yolor/data/coco.yaml \
     --names /jetson-yolor/data/coco.names \
     --img ${IMAGE_SIZE} --batch ${BATCH_SIZE} --conf 0.001 --iou 0.65 --device 0 \
