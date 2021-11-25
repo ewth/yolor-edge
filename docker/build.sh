91bf397a8b52b608538602d568c2aa1a234bc878
Ewan Thompson <ewanpt@gmail.com>
2021-11-25 12:47:37 +0800

    Reorder folders

--- docker/build.sh
+++ docker/build.sh
@@ -4,9 +4,14 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 TORCHV_WHL=torchvision-0.8.1-cp36-cp36m-linux_aarch64.whl
 
+cd ${SCRIPT_DIR}
+if [ -f ${TORCHV_WHL} ]; then
+    echo "Can't find file ${TORCH_WHEEL}"
+    exit
+fi
+
 sudo docker pull nvcr.io/nvidia/l4t-pytorch:r32.5.0-pth1.7-py3
 sudo DOCKER_BUILDKIT=1 docker build \
     --build-arg TORCHV_WHL=${TORCHV_WHL} \
     --progress plain \
-    -t jetson-yolor \
-    -f ${SCRIPT_DIR}/Dockerfile .
+    -t jetson-yolor
