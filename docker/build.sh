a318bbc8d4a34df0c6e46e7ad24c7b143d204781
Ewan Thompson <ewanpt@gmail.com>
2021-11-25 19:57:31 +0800

    Update build

--- docker/build.sh
+++ docker/build.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
+IMAGE_TAG:deep-usar/jetson-yolor:latest
+TORCHV_WHL=torchvision-0.8.1-cp36-cp36m-linux_aarch64.whl
+
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-if [[ "${SCRIPT_DIR}" != $(echo pwd) ]]; then
-    echo "Please run the script from the directory ${SCRIPT_DIR}"
-    exit
-fi
-if [[ ! -f "${SCRIPT_DIR}/Dockerfile" ]]; then
+
+cd $SCRIPT_DIR
+
+if [[ ! -f "Dockerfile" ]]; then
     echo "No Dockerfile found."
     exit
 fi
 
-TORCHV_WHL=torchvision-0.8.1-cp36-cp36m-linux_aarch64.whl
 
-cd ${SCRIPT_DIR}
 if [ -f ${TORCHV_WHL} ]; then
     echo "Can't find file ${TORCH_WHEEL}"
     exit
 fi
 
 sudo docker pull nvcr.io/nvidia/l4t-pytorch:r32.5.0-pth1.7-py3
+
 sudo DOCKER_BUILDKIT=1 docker build \
-    --build-arg TORCHV_WHL=${TORCHV_WHL} \
     --progress plain \
-    -t jetson-yolor
+    -t ${IMAGE_TAG} .
