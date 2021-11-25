ad43fb41fc8712f82422b16008994126d76657b9
Ewan Thompson <ewanpt@gmail.com>
2021-11-25 20:02:52 +0800

    Put wandb env in run, not build; define wandb dir; setup container files

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 SHM_SIZE=4gb
+IMAGE=deep-usar/jetson-yolor:version0.1
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Bit hacky... better way?
 URBASE=$(dirname $(dirname ${SCRIPT_DIR}))
@@ -10,9 +12,10 @@ RESOURCES=${URBASE}/resources
 JETSON_YOLOR=${URBASE}/jetson-yolor
 CONTAINER=${SCRIPT_DIR}/container-files
 YOLOR=${URBASE}/yolor
+WANDB_DIR=${RESOURCES}/wandb/jetson-yolor
 
-V4L2_DEVICES=" "
-
+# Cheers Dusty!
+V4L2_DEVICES=""
 for i in {0..9}
 do
    TARGET_DEV=/dev/video${i}
@@ -23,16 +26,32 @@ do
 done
 
 # Including displays to run from xrdp
+ADD_DISPLAY=""
+if [[ ! -z "${DISPLAY}" ]]; then
+   sudo xhost +si:localuser:root
+   ADD_DISPLAY="-e DISPLAY=${DISPLAY}"
+fi
 
-sudo xhost +si:localuser:root
+ENV_ARGS=""
+if [[ ! -z "${WANDB_API_KEY}" ]]; then
+    ENV_ARGS="${ENV_ARGS} -e WANDB_API_KEY=""${WANDB_API_KEY}"""
+fi
+if [[ ! -z "${WANDB_PROJECT}" ]]; then
+    ENV_ARGS="${ENV_ARGS} -e WANDB_PROJECT=""${WANDB_PROJECT}"""
+fi
+if [[ ! -z "${WANDB_ENTITY}" ]]; then
+    ENV_ARGS="${ENV_ARGS} -e WANDB_ENTITY=""${WANDB_ENTITY}"""
+fi
+
 sudo docker run --runtime nvidia \
-    -it --rm --security-opt  seccomp=unconfined --network host \
-    -e DISPLAY=${DISPLAY} \
+    -it --rm --security-opt  seccomp=unconfined --network host ${ADD_DISPLAY} \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /tmp/argus_socket:/tmp/argus_socket \
     -v /etc/enctune.conf:/etc/enctune.conf \
+    -v ${WANDB_DIR}:/wandb \
+    -e WANDB_DIR="/wandb" ${ENV_ARGS} \
     -v ${RESOURCES}:/resources \
-    -v ${JETSON_YOLOR}:/jetson-yolor \
+    -v ${CONTAINER}:/jetson-yolor \
     -v ${YOLOR}:/yolor \
     --shm-size=${SHM_SIZE} \
-    ${V4L2_DEVICES} jetson-yolor:latest
+    ${V4L2_DEVICES} ${IMAGE}
