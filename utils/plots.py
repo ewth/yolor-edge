1f5b6e604e305c4d18cb258e8b7d36459578244b
Ewan Thompson <ewanpt@gmail.com>
2021-11-28 20:53:39 +0800

    Change how boxes are plotted

--- utils/plots.py
+++ utils/plots.py
@@ -51,18 +51,22 @@ def butter_lowpass_filtfilt(data, cutoff=1500, fs=50000, order=5):
     return filtfilt(b, a, data)  # forward-backward filter
 
 
-def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+def plot_one_box(x, img, color=None, label=None, line_thickness=None, text_color=None, line_type=None):
     # Plots one bounding box on image img
+    if not line_type:
+        line_type = cv2.LINE_AA
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=line_type)
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        if not text_color:
+            text_color = [0, 0, 0]
+        cv2.rectangle(img, c1, c2, color, -1, line_type)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, text_color, thickness=tf, lineType=line_type)
 
 
 def plot_wh_methods():  # from utils.general import *; plot_wh_methods()
