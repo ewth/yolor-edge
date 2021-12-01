1466164e2312a0066ac43007bd36323d4b9d2012
Ewan Thompson <ewanpt@gmail.com>
2021-12-01 22:27:08 +0800

    Shuffle files around

--- scripts/val/run-val.sh
+++ scripts/val/run-val.sh
@@ -36,12 +36,12 @@ else
     SHM_SIZE=$((${SHM_SIZE::-1} / ${SHM_DIV}))
 fi
 
-SHM_SIZE=${SHM_SIZE} python3 /yolor/test.py \
+SHM_SIZE=${SHM_SIZE} python3 /yolor-edge/yolor/test.py \
     --verbose \
-    --data /jetson-yolor/data/coco.yaml \
-    --names /jetson-yolor/data/coco.names \
-    --img ${IMAGE_SIZE} --batch ${BATCH_SIZE} --conf 0.001 --iou 0.65 --device 0 \
-    --cfg /yolor/cfg/${YOLOR_VERSION}.cfg \
+    --data /yolor-edge/data/coco.yaml \
+    --names /yolor-edge/data/coco.names \
+    --img-sz ${IMAGE_SIZE} --batch ${BATCH_SIZE} --conf 0.001 --iou 0.65 --device 0 \
+    --cfg /yolor-edge/yolor/cfg/${YOLOR_VERSION}.cfg \
     --weights /resources/weights/yolor/${YOLOR_VERSION}.pt \
     --project /resources/runs/yolor/test \
     --name ${YOLOR_VERSION}_val
