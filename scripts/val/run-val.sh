0b99d18d1e6ba4012cece87f51aa11073ef6e51e
Ewan Thompson <ewanpt@gmail.com>
2021-12-03 09:38:46 +0800

    Update data, names, cfg refs

--- scripts/val/run-val.sh
+++ scripts/val/run-val.sh
@@ -10,11 +10,22 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 SINGLE_CLS=""
+DATASET="COCO-2017"
+# The test.py script reverts to these defaults too
+TEST_NAMES="/yolor-edge/data/coco-2017/coco.names"
+TEST_DATA="/yolor-edge/data/coco-2017/coco.yaml"
 
 if [[ ! -z "${DEEPRESCUE}" ]]; then
-    YOLOR_NAMES="/yolor-edge/data/deeprescue/coco.names"
-    YOLOR_DATA="/yolor-edge/data/deeprescue/coco.yaml"
-    SINGLE_CLS="--single-class"
+    if [[ "${DEEPRESCUE}" == "1" ]]; then
+        TEST_NAMES="/resources/datasets/deeprescue/v1/deeprescue.names"
+        TEST_DATA="/resources/datasets/deeprescue/v1/deeprescue.yaml"
+    fi
+    if [[ "${DEEPRESCUE}" == "2" ]]; then
+        TEST_NAMES="/resources/datasets/deeprescue/v2/deeprescue.names"
+        TEST_DATA="/resources/datasets/deeprescue/v2/data.yaml"
+    fi
+    SINGLE_CLS="--single-cls"
+    DATASET="deeprescue"
 fi
 
 if [[ ! -z "${BATCH_SIZE}" ]]; then
@@ -23,22 +34,18 @@ fi
 if [[ ! -z "${IMAGE_SIZE}" ]]; then
     IMAGE_SIZE="--img-sz ${IMAGE_SIZE}"
 fi
-if [[ ! -z "${YOLOR_DATA}" ]]; then
-    YOLOR_DATA="--data ${YOLOR_DATA}"
+if [[ ! -z "${TEST_DATA}" ]]; then
+    TEST_DATA="--data ${TEST_DATA}"
 fi
-if [[ ! -z "${YOLOR_NAMES}" ]]; then
-    YOLOR_NAMES="--names ${YOLOR_NAMES}"
+if [[ ! -z "${TEST_NAMES}" ]]; then
+    TEST_NAMES="--names ${TEST_NAMES}"
 fi
-if [[ -z "${YOLOR_VERSION}" ]]; then
-    YOLOR_VERSION=yolor_p6
+if [[ -z "${YOLOR_CFG}" ]]; then
+    YOLOR_CFG=yolor_p6
 fi
 
 
-if [[ ! -z "${DEEPRESCUE}" ]]; then
-    echo "Starting validation of ${YOLOR_VERSION} on deeprescue data"
-else
-    echo "Starting validation of ${YOLOR_VERSION} on COCO-2017 data"
-fi
+echo "Starting validation run of ${YOLOR_CFG} on ${DATASET} data"
 
 # This is just read to pass to wandb
 SHM_SIZE=$(cat /proc/mounts | grep "/dev/shm" | grep -Po 'size=([0-9]+[a-zA-Z])' | grep -Po '([0-9]+[a-zA-Z])')
@@ -55,9 +62,13 @@ else
     SHM_SIZE=$((${SHM_SIZE::-1} / ${SHM_DIV}))
 fi
 
+    # parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    # parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    # parser.add_argument('--save-json', action='store_true', help='save a cocoapi-compatible JSON results file')
+
 SHM_SIZE=${SHM_SIZE} python3 /yolor-edge/yolor/test.py \
-    ${IMAGE_SIZE} ${BATCH_SIZE} ${YOLOR_DATA} ${YOLOR_NAMES} ${SINGLE_CLS} --verbose \
-    --is-coco \
-    --cfg /yolor-edge/yolor/cfg/${YOLOR_VERSION}.cfg \
-    --weights /resources/weights/yolor/${YOLOR_VERSION}.pt \
-    --name ${YOLOR_VERSION}_val
+    ${IMAGE_SIZE} ${BATCH_SIZE} ${TEST_DATA} ${TEST_NAMES} ${SINGLE_CLS} --verbose \
+    --save-txt --save-conf --save-json \
+    --cfg /yolor-edge/yolor/cfg/${YOLOR_CFG}.cfg \
+    --weights /resources/weights/yolor/${YOLOR_CFG}.pt \
+    --name ${YOLOR_CFG}_val
