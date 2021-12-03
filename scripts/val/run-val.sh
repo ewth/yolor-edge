39c611d8535328260c692076c53e14829f94ec53
Ewan Thompson <ewanpt@gmail.com>
2021-12-03 13:52:33 +0800

    Update args and set quick run

--- scripts/val/run-val.sh
+++ scripts/val/run-val.sh
@@ -9,7 +9,6 @@
 # Thanks https://stackoverflow.com/questions/59895/
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-SINGLE_CLS=""
 DATASET="COCO-2017"
 # The test.py script reverts to these defaults too
 TEST_NAMES="/yolor-edge/data/coco-2017/coco.names"
@@ -24,21 +23,21 @@ if [[ ! -z "${DEEPRESCUE}" ]]; then
         TEST_NAMES="/resources/datasets/deeprescue/v2/deeprescue.names"
         TEST_DATA="/resources/datasets/deeprescue/v2/data.yaml"
     fi
-    SINGLE_CLS="--single-cls"
+    EXTRA_ARGS="${EXTRA_ARGS} --single-cls"
     DATASET="deeprescue"
 fi
 
 if [[ ! -z "${BATCH_SIZE}" ]]; then
-    BATCH_SIZE="--batch-size ${BATCH_SIZE}"
+    EXTRA_ARGS="${EXTRA_ARGS} --batch-size ${BATCH_SIZE}"
 fi
 if [[ ! -z "${IMAGE_SIZE}" ]]; then
-    IMAGE_SIZE="--img-sz ${IMAGE_SIZE}"
+    EXTRA_ARGS="${EXTRA_ARGS} --img-size ${IMAGE_SIZE}"
 fi
 if [[ ! -z "${TEST_DATA}" ]]; then
-    TEST_DATA="--data ${TEST_DATA}"
+    EXTRA_ARGS="${EXTRA_ARGS} --data ${TEST_DATA}"
 fi
 if [[ ! -z "${TEST_NAMES}" ]]; then
-    TEST_NAMES="--names ${TEST_NAMES}"
+    EXTRA_ARGS="${EXTRA_ARGS} --names ${TEST_NAMES}"
 fi
 if [[ -z "${YOLOR_CFG}" ]]; then
     YOLOR_CFG=yolor_p6
@@ -46,11 +45,14 @@ fi
 
 
 echo "Starting validation run of ${YOLOR_CFG} on ${DATASET} data"
+if [[ ! -z "${EXTRA_ARGS}" ]]; then
+    echo " with extra args ${EXTRA_ARGS}"
+fi
 
 # This is just read to pass to wandb
 SHM_SIZE=$(cat /proc/mounts | grep "/dev/shm" | grep -Po 'size=([0-9]+[a-zA-Z])' | grep -Po '([0-9]+[a-zA-Z])')
 if [[ ! $SHM_SIZE =~ [0-9]+[a-zA-Z] ]]; then
-    SHM_SIZE=""
+    SHM_SIZE="-1"
 else
     SHM_FACT=${SHM_SIZE: -1}
     SHM_DIV=1
@@ -66,9 +68,16 @@ fi
     # parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
     # parser.add_argument('--save-json', action='store_true', help='save a cocoapi-compatible JSON results file')
 
+# Quick run
 SHM_SIZE=${SHM_SIZE} python3 /yolor-edge/yolor/test.py \
-    ${IMAGE_SIZE} ${BATCH_SIZE} ${TEST_DATA} ${TEST_NAMES} ${SINGLE_CLS} --verbose \
-    --save-txt --save-conf --save-json \
+    --single-cls ${EXTRA_ARGS} \
     --cfg /yolor-edge/yolor/cfg/${YOLOR_CFG}.cfg \
     --weights /resources/weights/yolor/${YOLOR_CFG}.pt \
     --name ${YOLOR_CFG}_val
+
+# SHM_SIZE=${SHM_SIZE} python3 /yolor-edge/yolor/test.py \
+#     ${EXTRA_ARGS} --verbose \
+#     --save-txt --save-conf --save-json \
+#     --cfg /yolor-edge/yolor/cfg/${YOLOR_CFG}.cfg \
+#     --weights /resources/weights/yolor/${YOLOR_CFG}.pt \
+#     --name ${YOLOR_CFG}_val
