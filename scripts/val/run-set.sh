0c14579d48862ff7c7b40742000204f217280aa5
Ewan Thompson <ewanpt@gmail.com>
2021-12-08 02:31:56 +0800

    Performance validation A

--- scripts/val/run-set.sh
+++ scripts/val/run-set.sh
@@ -1,31 +1,52 @@
 #!/bin/bash
 
-# Now we want to explore:
-#	Infsize 512: Batch size 30, 31, 32, 33
-#	Infsize 128 and 256: Batch size 30, 32, 34, 40, 50
-#	Infsize 960: 2, 4, 6
+# Conclusion:
+# Best mAP: 1280 at bsz 1, 0.525126601	0.707319024
+# Best mAP/runtime tradeoff at 1024x1024, batch size 4
+# 2nd: 960, 4
+# 3rd: 768, 8
+# 4th: 640, 12
 
-SET_TAG="init_perf_eval_Batch_E"
+SET_TAG="pvalA"
 
-for BATCH_SIZE in 30 31 32 33 34
+
+# Best mAP
+IMAGE_SIZE=1280
+BATCH_SIZE=1
+for runs in {1...20}
 do
-	IMAGE_SIZE=512
+	WANDB_TAGS=${SET_TAG} IMAGE_SIZE=${IMAGE_SIZE} BATCH_SIZE=${BATCH_SIZE} ./run-val.sh
+done
+
+# best perf
+IMAGE_SIZE=1024
+BATCH_SIZE=1
+for runs in {1...20}
+do
+	WANDB_TAGS=${SET_TAG} IMAGE_SIZE=${IMAGE_SIZE} BATCH_SIZE=${BATCH_SIZE} ./run-val.sh
+done
+
+# 2nd best perf
+IMAGE_SIZE=960
+BATCH_SIZE=4
+for runs in {1...20}
+do
 	WANDB_TAGS=${SET_TAG} IMAGE_SIZE=${IMAGE_SIZE} BATCH_SIZE=${BATCH_SIZE} ./run-val.sh
 done
 
 
-for BATCH_SIZE in 30 32 34 40 50
+# 3rd best perf
+IMAGE_SIZE=768
+BATCH_SIZE=8
+for runs in {1...20}
 do
-	for IMAGE_SIZE in 128 256
-	do
-		WANDB_TAGS=${SET_TAG} IMAGE_SIZE=${IMAGE_SIZE} BATCH_SIZE=${BATCH_SIZE} ./run-val.sh
-	done
-done
-
-for BATCH_SIZE in 2 4 6 8
-do
-	IMAGE_SIZE=960
 	WANDB_TAGS=${SET_TAG} IMAGE_SIZE=${IMAGE_SIZE} BATCH_SIZE=${BATCH_SIZE} ./run-val.sh
 done
 
-
+# 4th best perf
+IMAGE_SIZE=640
+BATCH_SIZE=12
+for runs in {1...20}
+do
+	WANDB_TAGS=${SET_TAG} IMAGE_SIZE=${IMAGE_SIZE} BATCH_SIZE=${BATCH_SIZE} ./run-val.sh
+done
