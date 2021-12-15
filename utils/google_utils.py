1d01870bfa4889f2970e33182819196a03c4b834
Ewan Thompson <ewanpt@gmail.com>
2021-12-15 21:37:47 +0800

    Uncommenting attempt_load

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -35,20 +35,20 @@ def attempt_download(weights):
             print('')
             
             
-# def attempt_load(weights, map_location=None):
-#     # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
-#     model = Ensemble()
-#     for w in weights if isinstance(weights, list) else [weights]:
-#         attempt_download(w)
-#         model.append(torch.load(w, map_location=map_location)['model'].float().fuse().eval())  # load FP32 model
+def attempt_load(weights, map_location=None):
+    # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
+    model = Ensemble()
+    for w in weights if isinstance(weights, list) else [weights]:
+        attempt_download(w)
+        model.append(torch.load(w, map_location=map_location)['model'].float().fuse().eval())  # load FP32 model
 
-#     if len(model) == 1:
-#         return model[-1]  # return model
-#     else:
-#         print('Ensemble created with %s\n' % weights)
-#         for k in ['names', 'stride']:
-#             setattr(model, k, getattr(model[-1], k))
-#         return model  # return ensemble
+    if len(model) == 1:
+        return model[-1]  # return model
+    else:
+        print('Ensemble created with %s\n' % weights)
+        for k in ['names', 'stride']:
+            setattr(model, k, getattr(model[-1], k))
+        return model  # return ensemble
 
 
 def gdrive_download(id='1n_oKgR81BJtqk75b00eAjdv03qVCQn2f', name='coco128.zip'):
